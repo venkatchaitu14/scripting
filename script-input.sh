a627b9795ddc0eb0ca505e953955cb6ce497c64a
venkatchaitu14 <venketchaitu14@gmail.com>
2024-04-28 23:17:02 +0530

    script.sh

---
+++ script-input.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+echo "\$* value=$*"
+echo "\$@ value=$@"
+echo "\$0 value=$0"
+echo "\$1 value=$1"
+echo "\$2 value=$2"
+echo "\$3 value=$3"
+echo "\$4 value=$4"
+echo "\$# value=$#"
+echo "\$$ value=$$"
